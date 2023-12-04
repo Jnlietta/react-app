e1436d71c587009e2f700f8ed03bdeae9bcb63fa
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-04 15:11:47 +0100

    Add components Favorite and About

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,12 @@
+import styles from './Favorite.module.scss';
+
+const Favorite = () => {
+    return(
+        <div className={styles.hero}>
+            <h1 className={styles.title}>Favorite</h1>
+            <p>Lorem ipsum.</p>
+        </div>
+    );
+};
+
+export default Favorite;
