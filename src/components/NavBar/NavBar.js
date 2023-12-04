22f92485b39f8c98bca8f5deb286bfc4b2d99869
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-04 14:52:05 +0100

    Add package react-router, add component NavBar and change a bit Container

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container';
+
+const NavBar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <div className={styles.navbar__container}>
+                    <a href="/" className={styles.icon}>
+                        <i className="fa fa-tasks"></i>
+                    </a>
+                    <ul className={styles.navbar__list}>
+                        <li>
+                            <a href="/">home</a>
+                        </li>
+                        <li>
+                            <a href="/favorite">favorite</a>
+                        </li>
+                        <li>
+                            <a href="/about">about</a>
+                        </li>
+                    </ul>
+                </div>
+            </Container>
+        </nav>
+    );
+};
+
+export default NavBar;
