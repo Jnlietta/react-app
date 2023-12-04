06c9e52d45424480b12aa09e72be0532de789e8b
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-04 21:10:57 +0100

    Add components Routes and Route from package react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import Favorite from './components/Favorite/Favorite.js';
-import About from './components/About/About.js';;
+import About from './components/About/About.js';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
-        <Favorite />
-        <About />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
       </Container>
     </main>
   );
