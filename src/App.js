c75299870aa06aca6c9f78457ea2d87d600b5b0b
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-05 18:27:02 +0100

    Add component NotFound and use it in App - for every path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home.js';
 import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js';
 import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound.js';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
     </main>
