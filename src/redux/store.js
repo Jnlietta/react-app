ef5a500cbfde43a694cdcc458991a69812d88083
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-10 19:28:26 +0100

    Add action TOGGLE_CARD_FAVORITE

--- src/redux/store.js
+++ src/redux/store.js
@@ -26,6 +26,8 @@ export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', pay
 
 export const addList = payload => ({ type: 'ADD_LIST', payload });
 
+export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload});
+
 const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_COLUMN':
@@ -36,6 +38,8 @@ const reducer = (state, action) => {
       return { ...state, searchString: action.payload };
     case 'ADD_LIST':
       return { ...state, lists: [...state.lists, { id: shortid(), ...action.payload }]};
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card)};
     default:
       return state;
   }
