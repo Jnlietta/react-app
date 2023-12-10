82fa576ef5f26a363389f2fb4881e62897c41ef3
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-10 18:27:11 +0100

    Add component ListForm with styles file

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,32 @@
+import styles from './ListForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button.js';
+import TextInput from '../TextInput/TextInput.js';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store.js';
+
+const ListForm = () => {
+    const [title, setTitle] = useState ('');
+    const [description, setDescription] = useState ('');
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({ title, description }));        
+        setTitle('');
+        setDescription('');
+    };
+
+    return (
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <span className={styles.spanMargin}>Title:</span>
+            <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <span className={styles.spanMargin}>Description:</span>
+            <TextInput type="text" value={description} onChange={e => setDescription(e.target.value)} />
+            <Button>Add list</Button>
+        </form>
+	);
+};
+
+export default ListForm;
