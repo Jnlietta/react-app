fbe59fe2135c39f55433ab72baae219893477303
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-10 22:14:29 +0100

    Add package clsx, add local state isFavorite inside Card and add class to button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import styles from './Card.module.scss';
+import clsx from 'clsx';
 
 const Card = props => {
+
+    const [isFavorite, setIsFavorite] = useState(props.isFavorite);
+
+    console.log('isfav', isFavorite);
+
     return (
         <li className={styles.card}>
             {props.title}
-            <button type="button">
+            <button type="button" className={clsx(isFavorite && styles.isFav)} >
                 <i className="fa fa-star-o"></i>
             </button>
         </li>
