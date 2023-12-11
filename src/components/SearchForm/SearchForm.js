529d4f6a45a30148adbc0df471ec1489ba50c1e1
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-11 12:41:53 +0100

    Change in SearchForm to have in input always value of searchString from global state - error

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,24 +2,21 @@ import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
 import { getSearchString, updateSearchString } from '../../redux/store.js';
 
 const SearchForm = () => {
     const searchString = useSelector(getSearchString);
-    const [string, setString] = useState ('');
     
     const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch(updateSearchString(string));
-        setString('');
+        dispatch(updateSearchString(e.target.value));
     };
 
     return (
         <form className={styles.searchForm} onSubmit={handleSubmit}>
-            <TextInput type="text" value={string} onChange={e => setString(e.target.value)} placeholder={searchString} />
+            <TextInput type="text" value={searchString} onChange={handleSubmit} placeholder="Search..." />
             <Button>
                 <span className="fa fa-search" />
             </Button>
