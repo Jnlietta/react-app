9d7496591546466e2184bcffdc5d81e24278f9bc
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-12 17:19:27 +0100

    Add unique name of actions for columns

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import strContains from '../utils/strContains';
 import listsReducer from './listsRedux'
 import columnsReducer from './columnsRedux';
 import cardsReducer from './cardsRedux';
 import searchStringReducer from './searchStringRedux';
-
-//selectors
-
-
-export const getAllColumns = (state) => state.columns;
-
-export const getColumnsByList = ({columns}, listId) => columns.filter(columns => columns.listId === listId);
-
-
-
-
-// action creators
-export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-
-
-
 
 const subreducers = {
   lists: listsReducer,
